87e208ae8070ef44e7950693e873a58f7e49673a
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-13 22:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function dwarfRollCall(dwarves) {
   for(var i = 0; i < dwarves.length; i++) {
     arr.push(`${i + 1}. ${dwarves[i]}`);
   }
-  return arr.join(" ");
+  return arr.join(" ") + " ";
 }
 
 function summonCaptainPlanet(planeteerCalls){
